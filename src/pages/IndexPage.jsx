6d7d11fe95f3814ea144f7dd1b9d1cf124feaa4a
hajun <audgkwns44@gmail.com>
2022-07-04 15:02:13 +0900

    [Update]default layout of indexpage

--- src/pages/IndexPage.jsx
+++ src/pages/IndexPage.jsx
@@ -1,9 +1,43 @@
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 import Header from "../components/Header";
+
+const headerFontStyle = {
+  fontWeight: "bold",
+  fontSize: "200px",
+};
+
+const headerDetailStyle = {
+  fontSzie: "40px",
+};
 
 export default function MainPage() {
   return (
     <>
       <Header />
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          background:
+            "linear-gradient(to right, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)), url('deadlift.avif')",
+          backgroundSize: "cover",
+        }}
+      >
+        <Typography
+          variant="h1"
+          sx={{
+            color: "#FFF",
+            pl: "100vw",
+            pt: "100vh",
+            transform: "translate(-30vw, -50vh)",
+            ...headerFontStyle,
+          }}
+        >
+          AIT
+        </Typography>
+        <Typography variant="h2" sx={headerDetailStyle}></Typography>
+      </Box>
     </>
   );
 }
