dcd0f633560d397652d6066d1213a10d5f43b9ca
hajun <audgkwns44@gmail.com>
2022-07-04 14:05:29 +0900

    [Create]add pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import "./App.css";
-import Header from "./components/Header";
+import { createContext } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import IndexPage from "./pages/IndexPage";
+import AboutTeamPage from "./pages/AboutTeamPage";
+import AboutSolutionPage from "./pages/AboutSolutionPage";
+import IrPage from "./pages/IrPage";
+
+export const ThemeStateContext = createContext(true);
+// true means day-mode, false means dark-mode
 
 function App() {
   return (
-    <>
-      <Header />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<IndexPage />} />
+        <Route path="/aboutteam" exact element={<AboutTeamPage />} />
+        <Route path="/aboutsolution" exact element={<AboutSolutionPage />} />
+        <Route path="/ir" exact element={<IrPage />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/AboutSolutionPage.jsx
@@ -0,0 +1,9 @@
+import Header from "../components/Header";
+
+export default function AboutSolutionPage() {
+  return (
+    <>
+      <Header />
+    </>
+  );
+}
