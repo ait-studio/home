80f9b5ae365931e2b38b4664e972f901611cdb53
hajun <audgkwns44@gmail.com>
2022-07-05 14:43:00 +0900

    [Create]create footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import Grid from "@mui/material/Grid";
+
+import RoomIcon from "@mui/icons-material/Room";
+
+const footerWrapperStyle = {
+  p: "30px",
+  backgroundColor: "#DDD",
+  color: "#666",
+  fontStyle: "italic",
+  fontFamily: "cursive",
+};
+
+export default function Footer() {
+  return (
+    <Grid container sx={footerWrapperStyle}>
+      <Grid item xs={6} sx={{ textAlign: "left" }}>
+        <RoomIcon />
+        Location | KT&G 상상플래닛, 서울시 성동구 뚝섬로 13길 38 (성수동 2가)
+      </Grid>
+      <Grid item xs={6} sx={{ textAlign: "right" }}>
+        대표자 박신기 | 010-1234-5678 | mockupemail@google.com
+      </Grid>
+    </Grid>
+  );
+}
