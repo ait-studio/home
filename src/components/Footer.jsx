2d61917fa9445d08a388093a0b4a2996915a9eda
hajun <audgkwns44@gmail.com>
2022-09-08 15:38:17 +0900

    [Styling]Footer, apply alignItem

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 
 import RoomIcon from "@mui/icons-material/Room";
 
@@ -13,9 +14,15 @@ const footerWrapperStyle = {
 export default function Footer() {
   return (
     <Grid container sx={footerWrapperStyle}>
-      <Grid item xs={6} sx={{ textAlign: "left" }}>
+      <Grid
+        item
+        xs={6}
+        sx={{ textAlign: "left", display: "flex", alignItems: "center" }}
+      >
         <RoomIcon />
-        Location | 상상플래닛 707호, 서울시 성동구 뚝섬로 13길 38 (성수동 2가)
+        <Typography sx={{ ml: 1 }}>
+          Location | 상상플래닛 707호, 서울시 성동구 뚝섬로 13길 38 (성수동 2가)
+        </Typography>
       </Grid>
       <Grid item xs={6} sx={{ textAlign: "right" }}>
         대표자 박신기 | amazing601@google.com
