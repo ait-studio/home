73e3a8e06bc684372e026919fc7d501a965a0b83
hajun <audgkwns44@gmail.com>
2023-04-04 08:10:08 +0900

    [Update]layout for about-solution page

--- src/pages/AboutSolutionPage.jsx
+++ src/pages/AboutSolutionPage.jsx
@@ -6,40 +6,150 @@ import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Divider from "@mui/material/Divider";
 
+import ControlPointIcon from "@mui/icons-material/ControlPoint";
+
 export default function AboutSolutionPage() {
-  const mockupCards = [
-    {
-      type: "헬스장에 적용된 인공지능",
-      title: "똑똑한 헬스장, 합리적인 시스템",
-      tag: ["헬스", "보디빌딩", "Pose Model", "Skeleton Structure"],
-      content: [
-        "모션을 인식하고, 관절과 관절 사이를 강체(Rigid body)로 취급해 표현하는 스켈레톤 구조를 얻으려면, 기존에는 고가의 모션캡처 수트와 장비들이 필요했습니다",
-        "그러나 우리 AIT studio는 심도 카메라도, 모션캡처 장비도 없이 영상으로부터 스켈레톤 구조를 추출하는 딥러닝-경량 인공지능 기반의 플랫폼을 제공합니다",
-        "당신의 헬스장을 스마트 헬스장으로 업그레이드하세요",
-        "",
-        "사용자는 촬영되고 분석된 운동 영상을 손쉽게 받아볼 수 있고, 트레이너는 운동 피드백이라는 원래 역할에 집중할 수 있습니다",
-      ],
-    },
-  ];
   return (
     <>
       <Header />
-      <Box
-        sx={{
-          width: "calc(100vw - 100px)",
-          height: "calc(100vh - 385.5px)",
-          // padding 150px *2, footer 85.5px
-          minHeight: "calc(100vh - 391.5px)",
-          p: "150px 50px",
-        }}
-      >
-        <Typography variant="h1" sx={{ fontSize: "50px", mb: "50px" }}>
-          # our solutions
-        </Typography>
-        <Divider />
-        <CardGroup cards={mockupCards} number_of_cards_in_a_row={1} />
+      <Box sx={{ ...styles.bodySize, ...styles.background }}>
+        <Box sx={{ ...styles.centerize, ...styles.header }}>
+          <Typography sx={{ ...styles.headerText }}>Us Solutions</Typography>
+        </Box>
+        <Box sx={styles.cardWrapper}>
+          <Box
+            className="card"
+            sx={{ ...styles.card, ...styles.glassmorphism }}
+          >
+            <Typography sx={{ ...styles.cardTitle }}>FMMT</Typography>
+            <Typography sx={{ ...styles.cardSubTitle }}>
+              Fitness MoMenT, 헬스장의 ICT화
+            </Typography>
+            <Box sx={{ ...styles.cardThumbnail }}>
+              Image for Represent this Item
+            </Box>
+            <Box
+              sx={{ ...styles.moreButton }}
+              onClick={() => {
+                alert("Modal window activator will come to here");
+              }}
+            >
+              <ControlPointIcon />
+            </Box>
+          </Box>
+          <Box
+            className="card"
+            sx={{ ...styles.card, ...styles.glassmorphism }}
+          >
+            <Typography sx={{ ...styles.cardTitle }}>FMMT for Edu</Typography>
+            <Typography sx={{ ...styles.cardSubTitle }}>
+              FMMT for Education, 교육을 위한 ICT 장비
+            </Typography>
+            <Box sx={{ ...styles.cardThumbnail }}>
+              Image for Represent this Item
+            </Box>
+            <Box
+              sx={{ ...styles.moreButton }}
+              onClick={() => {
+                alert("Modal window activator will come to here");
+              }}
+            >
+              <ControlPointIcon />
+            </Box>
+          </Box>
+          <Box
+            className="card"
+            sx={{ ...styles.card, ...styles.glassmorphism }}
+          >
+            <Typography sx={{ ...styles.cardTitle }}>PD App</Typography>
+            <Typography sx={{ ...styles.cardSubTitle }}>
+              Parkinson Disease App, 파킨슨 초기 진단장치
+            </Typography>
+            <Box sx={{ ...styles.cardThumbnail }}>
+              Image for Represent this Item
+            </Box>
+            <Box
+              sx={{ ...styles.moreButton }}
+              onClick={() => {
+                alert("Modal window activator will come to here");
+              }}
+            >
+              <ControlPointIcon />
+            </Box>
+          </Box>
+        </Box>
       </Box>
       <Footer />
     </>
   );
 }
+
+const styles = {
+  bodySize: {
+    width: "100vw",
+    height: "calc(100vh - 85.5px)",
+    // padding 150px *2, footer 85.5px
+    minHeight: "calc(100vh - 391.5px)",
+    p: "150px 50px",
+    boxSizing: "border-box",
+  },
+  background: {
+    background: "url('/deadlift.avif')",
+    backgroundSize: "cover",
+  },
+  centerize: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  header: {
+    width: "100%",
+    height: "10%",
+    backgroundColor: "#FFF",
+    borderRadius: "20px",
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 34,
+  },
+  cardTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  cardSubTitle: {
+    fontSize: 20,
+  },
+  card: {
+    // width: "max-content",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    m: 1,
+    p: 1.5,
+  },
+  glassmorphism: {
+    backdropFilter: "blur(16px) saturate(180%)",
+    backgroundColor: "rgba(255, 255, 255, 0.75)",
+    borderRadius: "12px",
+    border: "1px solid rgba(209, 213, 219, 0.3)",
+  },
+  cardWrapper: {
+    display: "flex",
+    paddingTop: 3,
+    justifyContent: "space-between",
+  },
+  cardThumbnail: {
+    width: 400,
+    height: 300,
+    backgroundColor: "#F00",
+    borderRadius: 5,
+  },
+  moreButton: {
+    m: 1,
+    mb: 0,
+    "&:hover": {
+      cursor: "pointer",
+    },
+  },
+};
